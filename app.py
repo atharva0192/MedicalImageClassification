d98d297d36098189611120d1364178475f24e68f
CodeBreaker <123104446+atharva0192@users.noreply.github.com>
2023-12-27 15:38:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,8 +35,6 @@ if uploaded_file is not None:
     # Display the prediction results
     st.write("Prediction Results:")
     if result>=0.5 :
-        st.write("Normal")
-        st.write(1)
+        st.write("Normal : 1")
     else:
-        st.write("Normal")
-        st.write(0)
+        st.write("Infected : 0")
