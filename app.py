5bfcd5ae041a1e6b39d9f2df1dc7bd1c54ab93d7
CodeBreaker <123104446+atharva0192@users.noreply.github.com>
2023-12-28 17:32:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,16 @@ st.sidebar.title("Classification of Medical X-Rays")
 st.sidebar.write(
     "In recent years, the intersection of medical imaging and deep learning has witnessed unprecedented advancements, revolutionizing the landscape of healthcare. One notable application that has gained substantial attention is medical image classification using Convolutional Neural Networks (CNNs). As we embark on this project, we delve into the realm of leveraging cutting-edge deep learning techniques to augment traditional medical image analysis"
 )
-
+st.markdown(
+    f'''
+        <style>
+            .sidebar .sidebar-content {{
+                width: 400px;
+            }}
+        </style>
+    ''',
+    unsafe_allow_html=True
+)
 
 # Main content
 
