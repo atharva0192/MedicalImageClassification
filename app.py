d1a79d5ec5aa660aaf30ab036a18170732d51421
CodeBreaker <123104446+atharva0192@users.noreply.github.com>
2023-12-28 15:50:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,28 @@ if uploaded_file is not None:
     # Perform prediction
     if st.button("Predict"):
         result = predict(uploaded_file)
-
+        progress_bar = st.progress(0)
+        status_text = st.empty()
+        chart = st.line_chart(np.random.randn(10, 2))
+        
+        for i in range(100):
+            # Update progress bar.
+            progress_bar.progress(i + 1)
+        
+            new_rows = np.random.randn(10, 2)
+        
+            # Update status text.
+            status_text.text(
+                'The latest random number is: %s' % new_rows[-1, 1])
+        
+            # Append data to the chart.
+            chart.add_rows(new_rows)
+        
+            # Pretend we're doing some computation that takes time.
+            time.sleep(0.1)
+        
+        status_text.text('Done!')
+        st.balloons()
         # Display the prediction results
         st.write("Prediction Results:")
         prediction_label = "Normal" if result >= 0.5 else "Infected"
