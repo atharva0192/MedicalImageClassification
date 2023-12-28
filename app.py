dc4a6f1ce0d83d6c8670911569a8e3955f93d58d
CodeBreaker <123104446+atharva0192@users.noreply.github.com>
2023-12-28 17:23:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,19 +54,18 @@ st.set_page_config(
 )
 
 # Sidebar
+with st.sidebar: 
+    st_lottie(back_json , 
+              height=400, 
+              width=400)
 st.sidebar.title("Classification of Medical X-Rays")
 st.sidebar.write(
     "In recent years, the intersection of medical imaging and deep learning has witnessed unprecedented advancements, revolutionizing the landscape of healthcare. One notable application that has gained substantial attention is medical image classification using Convolutional Neural Networks (CNNs). As we embark on this project, we delve into the realm of leveraging cutting-edge deep learning techniques to augment traditional medical image analysis"
 )
 
-with st.sidebar: 
-    st_lottie(url_json , 
-              height=200, 
-              width=200)
+
 # Main content
-st_lottie(back_json , 
-              height=400, 
-              width=400)
+
 st.title("Classification of Medical X-Rays")
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
